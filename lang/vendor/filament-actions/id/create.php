d9887a160e6bbb964a7376a97c4b9a9036c5ced0
private210 <pname210@gmail.com>
2025-06-18 09:26:51 +0700

    language id

---
+++ lang/vendor/filament-actions/id/create.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Tambah :label',
+
+        'modal' => [
+
+            'heading' => 'Tambah :label',
+
+            'actions' => [
+
+                'create' => [
+                    'label' => 'Buat',
+                ],
+
+                'create_another' => [
+                    'label' => 'Buat & buat lainnya',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'created' => [
+                'title' => 'Data berhasil dibuat',
+            ],
+
+        ],
+
+    ],
+
+];
